016069645b76e51e81d090338c3ec08aa44b7beb
WangWeicheng <1198759369@qq.com>
2024-10-02 22:05:39 +0800

    finish quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -29,14 +29,23 @@ pub enum Command {
 }
 
 mod my_module {
+    use std::{io::repeat, string};
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output:Vec<String>  = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let mut s = String::from(string);
+            match command {
+                Command::Append(n) => s = string.to_owned()+&"bar".repeat(*n),
+                Command::Trim => s = (*string.trim()).to_string(),
+                Command::Uppercase => s = string.to_uppercase()
+            };
+            output.push(s);
         }
         output
     }
@@ -45,7 +54,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
